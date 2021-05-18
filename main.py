30af68751c0318aaf517c9da2c5761f25b648424
Programmēšana VVS <>
2021-05-18 10:56:35 +0000

    Faila tagad_taisam.txt pievienošana

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ f = open("teksts.txt")  #atveram failu
 saturs = f.read()       #nolasām faila saturu
 f.close()               #aizveram failu
 print(saturs)           #izvadām faila saturu
+
+
+z = open("tagad_taisam.txt", "w")  #faila izveidošana
+z.write("Tev sanāca arī šis!")
+z.close()
